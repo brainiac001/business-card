543468cd54294b82b209626ac9503097129e47e8
Oluwadamilare Idowu Joshua <oluwadamilareidowujoshua@gmail.com>
2022-10-29 03:09:16 +0100

    Updates

---
+++ precache-manifest.30d0afe44d15656e4c2177edecab3f45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c2e7ba7d6d550609c53",
+    "url": "/business-card/static/css/main.d8312302.chunk.css"
+  },
+  {
+    "revision": "8c2e7ba7d6d550609c53",
+    "url": "/business-card/static/js/main.cf279da8.chunk.js"
+  },
+  {
+    "revision": "242308b2bdf8527e882f",
+    "url": "/business-card/static/js/runtime~main.37217fa9.js"
+  },
+  {
+    "revision": "ca8562f04f27eeb964b0",
+    "url": "/business-card/static/js/2.a77e7803.chunk.js"
+  },
+  {
+    "revision": "11d5b9d5be061bfd7000",
+    "url": "/business-card/static/js/3.479c0d5f.chunk.js"
+  },
+  {
+    "revision": "01e76464b8f668280ad6fbee480dcc91",
+    "url": "/business-card/static/media/main.01e76464.jpeg"
+  },
+  {
+    "revision": "8e4d2b0456baae45957edc5e41b7d407",
+    "url": "/business-card/index.html"
+  }
+];
